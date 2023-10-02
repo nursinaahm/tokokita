e27c56d6144b95c66c319b60608f9b99ada70b61
Nursina Hamdalah <nursinahamdalah21@gmail.com>
2023-10-02 20:29:09 +0700

    tokokita

---
+++ lib/ui/login_page.dart
@@ -0,0 +1,120 @@
+// ignore_for_file: unused_field
+
+import 'package:flutter/material.dart';
+import 'package:tokokita/ui/registrasi_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
+
+  final _emailTextboxController = TextEditingController();
+  final _passwordTextboxController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            const Text('Login'),
+            const SizedBox(
+                width: 20), 
+            Text(
+              'created by Amda',
+              style: TextStyle(
+                fontSize: 12,
+                fontStyle: FontStyle
+                    .italic, 
+                color:
+                    Colors.white, 
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                _emailTextField(),
+                _passwordTextField(),
+                _buttonLogin(),
+                const SizedBox(
+                  height: 30,
+                ),
+                _menuRegistrasi(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _emailTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(labelText: "Email"),
+      keyboardType: TextInputType.emailAddress,
+      controller: _emailTextboxController,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Email harus diisi';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _passwordTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(labelText: "Password"),
+      keyboardType: TextInputType.text,
+      obscureText: true,
+      controller: _passwordTextboxController,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return "Password harus diisi";
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buttonLogin() {
+    return ElevatedButton(
+      child: const Text("Login"),
+      onPressed: () {
+        // ignore: unused_local_variable
+        var validate = _formKey.currentState!.validate();
+      },
+    );
+  }
+
+  Widget _menuRegistrasi() {
+    return Center(
+      child: InkWell(
+        child: const Text(
+          "Registrasi",
+          style: TextStyle(color: Colors.blue),
+        ),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const RegistrasiPage()),
+          );
+        },
+      ),
+    );
+  }
+}
